741f7d4f171f601784e718b2e9c253b9e96c2b26
Caroline Peri <caroline.anne.peri@gmail.com>
2023-04-20 23:42:59 -0400

    feat: add integer requirement to validations on bulk discount model

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -2,6 +2,6 @@ class BulkDiscount < ApplicationRecord
   belongs_to :merchant
   has_many :items, through: :merchant
 
-  validates :percentage, presence: true, numericality: true
-  validates :threshold_quantity, presence: true, numericality: true
+  validates :percentage, presence: true, numericality: { only_integer: true }
+  validates :threshold_quantity, presence: true, numericality: { only_integer: true }
 end
