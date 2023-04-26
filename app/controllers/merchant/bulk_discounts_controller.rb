484c2ee3280d730045222c253d557c3ec55257b6
Caroline Peri <caroline.anne.peri@gmail.com>
2023-04-25 20:52:27 -0400

    feat: update controller workflow to handle invalid editing of bulk discounts

--- app/controllers/merchant/bulk_discounts_controller.rb
+++ app/controllers/merchant/bulk_discounts_controller.rb
@@ -11,16 +11,16 @@ class Merchant::BulkDiscountsController < ApplicationController
 
   def create
     discount = BulkDiscount.new(bulk_discount_params.merge(merchant_id: @merchant.id))
-    if valid_discount?
+    if invalid_discount?
+      flash.now[:alert] = "Try again! You cannot create a discount that will not be applied."
+      render :new
+    else
       if discount.save
         redirect_to merchant_bulk_discounts_path(@merchant), notice: "Discount successfully added!"
       else
         flash.now[:alert] = "Error: #{error_message(discount.errors)}"
         render :new
       end
-    else
-      flash.now[:alert] = "Try again! You cannot create a discount that will not be applied."
-      render :new
     end
   end
 
@@ -32,8 +32,13 @@ class Merchant::BulkDiscountsController < ApplicationController
 
   def update
     if attributes_changed?
-      @discount.update(bulk_discount_params)
-      redirect_to merchant_bulk_discount_path(@merchant, @discount), notice: "Discount successfully updated!"
+      if invalid_discount?
+        flash.now[:alert] = "Try again! The attempted update would make this discount invalid."
+        render :edit
+      else
+        @discount.update(bulk_discount_params)
+        redirect_to merchant_bulk_discount_path(@merchant, @discount), notice: "Discount successfully updated!"
+      end
     else
       flash.now[:alert] = "Error: You must update at least 1 field to continue."
       render :edit
@@ -58,13 +63,13 @@ class Merchant::BulkDiscountsController < ApplicationController
     params.require(:bulk_discount).permit(:percentage, :threshold_quantity)
   end
 
-  def valid_discount?
+  def invalid_discount?
     current_discount = @merchant.lowest_quantity_discount
     current_percentage = current_discount.percentage
     current_quantity = current_discount.threshold_quantity
 
-    (bulk_discount_params[:percentage].to_i > current_percentage) ||
-    (bulk_discount_params[:threshold_quantity].to_i < current_quantity)
+    (bulk_discount_params[:percentage].to_i < current_percentage) &&
+    (bulk_discount_params[:threshold_quantity].to_i >= current_quantity)
   end
 
   def attributes_changed?
